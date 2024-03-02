b36a1c82a834ba82dc118d518a1a17ffa10dde7f
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-03-02 22:16:05 +0530

    done with the hardware and phase 1 of the project

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,11 +2,19 @@ static boolean IsLightOn = false;
 void setup() {
   Serial.begin(9600);
   pinMode(7, INPUT);
+  pinMode(6, INPUT);
+  pinMode(5, INPUT);
+  pinMode(4, INPUT);
+  pinMode(3, INPUT);
   pinMode(8, OUTPUT);
 }
 
 void loop() {
   int light = digitalRead(7);
+  int shutDown = digitalRead(6);
+  int sendMessage = digitalRead(5);
+  int LeetCodeDailyQuestion = digitalRead(4);
+  int goIncoginto = digitalRead(3);
   if(light==0){
     IsLightOn = !IsLightOn;
   }
@@ -16,5 +24,22 @@ void loop() {
   else{
     digitalWrite(8, LOW);
   }
+
+  if(shutDown==0){
+    Serial.println("shutdown");
+  }
+
+  if(sendMessage==0){
+    Serial.println("send");
+  }
+
+  if(LeetCodeDailyQuestion==0){
+    Serial.println("leetcode");
+  }
+
+  if(goIncoginto==0){
+    Serial.println("hide");
+  }
+
   delay(250);
 }
